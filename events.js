4f894d72d21858373129a9ba5d7e3bcd57bdd9c7
sa77irak <sa77irak@gmail.com>
2019-09-13 11:09:16 +0900

    added comments about events.js

--- events.js
+++ events.js
@@ -1,6 +1,8 @@
+// when we are trying to use our custom events in our application.
 const EventEmitter = require("events");
 const http = require("http");
 
+// extending the eventemitter class is how different node modules like http, fs and many other node core modules implements events internally. all of them actually inherit from the eventemitter class.
 class Sales extends EventEmitter {
   constructor() {
     super();
@@ -21,18 +23,23 @@ myEmitter.on("newSale", stock => {
   console.log(`There are now ${stock} items left in stock.`);
 });
 
+// we emit the event ourself. we can pass additional argument as well.
 myEmitter.emit("newSale", 9);
 
 //////////////////////////////////////////////////////////////////////////////////
+// as soon as there is a new request, the server automatically emits the request object.
+// if we are using a built-in node module, the functions in there will many times emit there own events and we only have to listen to them.
 
 const server = http.createServer();
 
+// first event listener
 server.on("request", (req, res) => {
   console.log("Request received!");
   console.log(req.url);
   res.end("Request received");
 });
 
+// second event listener
 server.on("request", (req, res) => {
   console.log("Another request :D");
 });
@@ -41,6 +48,7 @@ server.on("close", () => {
   console.log("Server closed");
 });
 
+// start server
 server.listen(8000, "127.0.0.1", () => {
   console.log("Waiting for requests...");
 });
